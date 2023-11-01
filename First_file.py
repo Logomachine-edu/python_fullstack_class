a588a6cdd396f1478190aabf235d3b3b53077642
Tanya <angel1237@gmail.com>
2023-11-01 21:20:17 +0300

    developer 2 startged

--- First_file.py
+++ First_file.py
@@ -1,7 +1,16 @@
 # Мой первый код на Python
+<<<<<<< HEAD
 print('Hello, cats 1!')
 print('Hello, cats 2!')
 print('Hello, cats 3!')
 
 # этот код работает как надо!
 # автор Танюшенька
+=======
+print('Hello, dogs 1!')
+print('Hello, dogs 2!')
+print('Hello, dogs 3!')
+print('Hello, dogs 4!')
+print('Hello, dogs 5!')
+# => Hello, World!
+>>>>>>> developer 2 startged

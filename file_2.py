062290f34722e1e0fbd47d6e72bc199e5ec77781
Ksenia Pestova <liberty.in.smile@yandex.ru>
2023-11-14 17:26:26 +0300

    dev_2 finished

--- file_2.py
+++ file_2.py
@@ -1,2 +1,10 @@
 #Второй код на питон
-print('Hello dogs!')
+print('Hello dogs 1!')
+print('Hello dogs 2!')
+print('Hello dogs 3!')
+print('Hello dogs 4!')
+print('Hello dogs 5!')
+print('Hello dogs 6!')
+
+#Задание закончено
+#Автор - dev_2

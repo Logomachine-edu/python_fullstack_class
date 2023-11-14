68509ab696cedb4772c60eadc4d13c9c640cf607
Rama <aduevramazan@gmail.com>
2023-11-14 14:38:23 +0300

    module_2_lesson_1_finished

--- First_file.py
+++
@@ -1,7 +0,0 @@
-#мой первый код на Pyton
-print ('Hello dogs! 1')
-print ('Hello dogs! 2')
-print ('Hello dogs! 3')
-
-# этот код работает как надо!
-# автор Rama
--- Second_file.py
+++
@@ -1,9 +0,0 @@
-#тут будет мой второй код на Pyton
-print ('Hello cats 1!')
-print ('Hello cats 2!')
-print ('Hello cats 3!')
-print ('Hello cats 4!')
-print ('Hello cats 5!')
-
-# этот код работатет испаравно
-# автор - разработчик #2

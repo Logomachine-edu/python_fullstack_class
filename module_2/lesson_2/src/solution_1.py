910d1c9342a06722b241ff2f78819442c4842c3c
Alexandr <you@example.comalex.7370@mail.ru>
2023-11-27 14:57:40 +0300

    module 2 tasks 2 and 3 are done

--- module_2/lesson_2/src/solution_1.py
+++ module_2/lesson_2/src/solution_1.py
@@ -1,6 +1,12 @@
-# число
-number: int = 2
+# запрашиваем у пользователя строку
+number_str = input("Введите число(строка):")
+# преобразуем строку в целое число
+number_true = int(number_str)
+#умножаем на два
+number_double = number_true*2
+#преобразуем в формат строки и выводим
+print(str(number_double))
 
-# строка
-
-my_name: str = "Alexandr"
+# Оно же, но в одну строку
+#number_str = str(2*int(input("Введите число:")))
+#print(type(number_str))

0e1daab722c9150014df909ef17a858954a85d80
Alexandr <alex.7370@mail.ru>
2023-12-04 17:50:10 +0300

    lesson 11 module 2 is done

---
+++ module_2/lesson_11/src/solution_1.py
@@ -0,0 +1,13 @@
+numbers_test = [100,200,300]
+numbers_test2 = [15,23,39,50]
+
+
+def sum_sales_with_for (lst):
+    sum = 0
+    for i in lst:
+        sum += i
+        print(i , end="+")
+    print(sum)
+
+
+sum_sales_with_for(numbers_test)

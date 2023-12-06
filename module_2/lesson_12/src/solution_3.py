88c9d9b8a3c51ffc6579955ec22e869e32ac55d7
Alexandr <alex.7370@mail.ru>
2023-12-06 11:33:03 +0300

    has problem with solution 3

--- module_2/lesson_12/src/solution_3.py
+++ module_2/lesson_12/src/solution_3.py
@@ -1,15 +1,19 @@
 data_sets = [
-    ([10, 23, 35, 47], # фильтрация нечётных значений),
+    (sorted([10, 23, 35, 47], key = lambda x: (x%2 == 1))),
 
-    ([5, 7, 8, 10], # значения больше 7),
+    (sorted([5, 7, 8, 10], key = lambda x: (x > 7))),
 
-    ([1, 2, 3, 5, 6], # значения меньше 5),
+    (sorted([1, 2, 3, 5, 6], key = lambda x: (x < 5))),
 
-    ([10, 20, 30, 40, 50], # только значения, кратные 5 и 8)
+    (sorted([10, 20, 30, 40, 50], key = lambda x: (x%8 == 0 or x%8 == 0))) 
 ]
 
 
 
 for data, filter_func in data_sets:
+    def filter_data(data,filter_func):
+        return filter_func(data)    
 
     filtered_data = filter_data(data, filter_func)
+
+print(filtered_data)

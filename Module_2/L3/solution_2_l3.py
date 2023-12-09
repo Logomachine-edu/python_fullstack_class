6e5e339747477619d6b98cf3c4070b5635f51635
Alexey <110417908+Hushfort@users.noreply.github.com>
2023-12-09 23:59:23 +0300

    Update solution_2_l3.py

--- Module_2/L3/solution_2_l3.py
+++ Module_2/L3/solution_2_l3.py
@@ -1,7 +1,6 @@
-day = int(input())
-even = 'В четные дни работают: Света Олег'
-odd = 'В нечетные дни работают: Маша Паша'
-if day % 2 == 0:
-    print(even)
-else:
-    print(odd)
+staff = 'Света', 'Маша', 'Олег', 'Паша'
+even = staff[::2]
+odd = staff[1::2]
+even_d = " ".join(even)
+odd_d = " ".join(odd)
+print('В четные дни работают:', even_d, '\n \n''В нечетные дни работают:', odd_d)

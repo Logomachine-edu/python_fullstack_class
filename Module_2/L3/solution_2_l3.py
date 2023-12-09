08456e36738ca23f43b38101ed216bafdaf18aef
Alexey <110417908+Hushfort@users.noreply.github.com>
2023-12-10 00:00:08 +0300

    Update solution_2_l3.py

--- Module_2/L3/solution_2_l3.py
+++ Module_2/L3/solution_2_l3.py
@@ -3,4 +3,4 @@ even = staff[::2]
 odd = staff[1::2]
 even_d = " ".join(even)
 odd_d = " ".join(odd)
-print('В четные дни работают:', even_d, '\n \n''В нечетные дни работают:', odd_d)
+print('В четные дни работают:', even_d, '\n \n' 'В нечетные дни работают:', odd_d)

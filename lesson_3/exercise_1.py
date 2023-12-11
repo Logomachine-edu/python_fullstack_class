bd1afa5feef92e814ab7b762cce601d34719a936
Nizametdinov Bulat <nizametdinov.bulat@mail.ru>
2023-12-11 15:41:23 +0300

    lesson3

---
+++ lesson_3/exercise_1.py
@@ -0,0 +1,3 @@
+products = [1, 14, 4, 8, 6, 9]
+Sum_products = sum(products)
+print(Sum_products)
---
+++ lesson_3/exercise_2.py
@@ -0,0 +1,7 @@
+Sweta = "Света"
+Oleg = "Олег"
+Masha = "Маша"
+Pasha = "Паша"
+print("В четные дни работают:", Sweta, Oleg, sep=" ")
+print()
+print("В нечетные дни:", Masha, Pasha, sep=" ")

306d2bded4a5481b1728b8d5533a0e20b6334299
Nizametdinov Bulat <nizametdinov.bulat@mail.ru>
2023-12-11 16:19:45 +0300

    lesson4

---
+++ lesson_4/exercise_1.py
@@ -0,0 +1,10 @@
+digital_1 = 42.01
+digital_2 = 42.99
+digital_3 = -0.6
+digital_4 = 0.4
+digital_5 = 5.5
+print(round(digital_1))
+print(round(digital_2))
+print(round(digital_3))
+print(round(digital_4))
+print(round(digital_5))

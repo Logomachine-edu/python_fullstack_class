2729d6a50687b5cb856768d70136ee407d8b63ab
Nizametdinov Bulat <nizametdinov.bulat@mail.ru>
2023-12-12 15:48:06 +0300

    fixed erorr

--- lesson_3/exercise_2.py
+++ lesson_3/exercise_2.py
@@ -1,7 +1,4 @@
-Sweta = "Света"
-Oleg = "Олег"
-Masha = "Маша"
-Pasha = "Паша"
-print("В четные дни работают:", Sweta, Oleg, sep=" ")
+people = ["Sweta", "Masha", "Oleg", "Pasha"]
+print("В четные дни работают:", people[0], people[2])
 print()
-print("В нечетные дни:", Masha, Pasha, sep=" ")
+print("В нечетные дни:", people[1], people[3])

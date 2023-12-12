39cbb9831f736e88af72859c9758ee8416bce0ea
Nizametdinov Bulat <nizametdinov.bulat@mail.ru>
2023-12-12 21:27:28 +0300

    bool()

--- lesson_4/exercise_4.py
+++ lesson_4/exercise_4.py
@@ -2,21 +2,7 @@ digital_1 = 0
 digital_2 = 10
 digital_3 = -5
 digital_4 = 0.1
-flag1 = True
-flag2 = False
-if digital_1 != 0:
-    print("Программа работает верно:", flag1)
-else:
-    print("Программа работает верно:", flag2)
-if digital_2 != 0:
-    print("Программа работает верно:", flag1)
-else:
-    print("Программа работает верно:", flag2)
-if digital_3 != 0:
-    print("Программа работает верно:", flag1)
-else:
-    print("Программа работает верно:", flag2)
-if digital_4 != 0:
-    print("Программа работает верно:", flag1)
-else:
-    print("Программа работает верно:", flag2)
+print("Программа работает верно:", bool(digital_1))
+print("Программа работает верно:", bool(digital_2))
+print("Программа работает верно:", bool(digital_3))
+print("Программа работает верно:", bool(digital_4))

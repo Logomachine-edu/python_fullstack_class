5b37c8ba999f0c6c93edff90c8fd08f5304b88fb
Nizametdinov Bulat <nizametdinov.bulat@mail.ru>
2023-12-12 21:18:03 +0300

    use slices

--- lesson_3/exercise_2.py
+++ lesson_3/exercise_2.py
@@ -1,4 +1,4 @@
 people = ["Sweta", "Masha", "Oleg", "Pasha"]
-print("В четные дни работают:", people[0], people[2])
+print("В четные дни работают:", people[::2])
 print()
-print("В нечетные дни:", people[1], people[3])
+print("В нечетные дни:", people[1::2])

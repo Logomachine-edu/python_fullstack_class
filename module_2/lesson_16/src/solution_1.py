d356299b79babb84cb65d43f8558e0a7d21b7b5a
Alexandr <alex.7370@mail.ru>
2023-12-27 17:02:03 +0300

    lesson 16 module 2 is started

---
+++ module_2/lesson_16/src/solution_1.py
@@ -0,0 +1,7 @@
+quantity = int(input("Введите количество полок:"))
+longs_arr = list(input("Введите длины полок через запятую:").split(","))
+longs = [int(x) for x in longs_arr]
+
+a = [[longs[i], 0] for i in range(quantity)]
+print(quantity)
+
---
+++ module_2/lesson_16/src/solution_2.py
@@ -0,0 +1,6 @@
+shelfs = [4, 5, 6]
+products = [2, 3, 5]
+
+
+a = [[shelfs[i], products[i]] for i in range(len(shelfs))]
+print(a)

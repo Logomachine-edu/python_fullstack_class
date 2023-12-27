2ff19478555869d845b33ed409f6c4f76e8709cc
Alexandr <alex.7370@mail.ru>
2023-12-27 14:57:18 +0300

    lesson 15 in module 2 is done

--- module_2/lesson_14/src/solution_1.py
+++ module_2/lesson_14/src/solution_1.py
@@ -1,11 +1,13 @@
 from typing import Iterator
 
+
 def gen() -> Iterator[int]:
     res = []
     for x in range(51):
         if x == 0 or x % 5 == 0:
-            a = (x,x,x)
+            a = (x, x, x)
             res.append(a)
     return print(res)
+
 
 gen()

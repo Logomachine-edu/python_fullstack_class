0b24a219d227a372a1da5fbe7965235f84f438ff
Nizametdinov Bulat <nizametdinov.bulat@mail.ru>
2024-01-03 20:35:18 +0300

    fixed 5 and 8

--- lesson_6/exercise_5.py
+++ lesson_6/exercise_5.py
@@ -2,7 +2,7 @@ def swapping_min_max(price):
     index_max = price.index(max(price))
     index_min = price.index(min(price))
     price[index_max], price[index_min] = price[index_min], price[index_max]
-    print(price)
+    print("Новые цены:", price)
 
 
 swapping_min_max([50, 45, 30, 25])

2e5119f4c61714e21e0c9163970d5f062089fc23
Nizametdinov Bulat <nizametdinov.bulat@mail.ru>
2024-01-10 22:08:24 +0300

    fixed reverse string

--- lesson_7/exercise_1.py
+++ lesson_7/exercise_1.py
@@ -1,7 +1,8 @@
-def reverse_string(text):
+def reverse_string(text, text2):
     text_reverse = text[::-1]
-    print(text_reverse)
+    text_reverse2 = text2[::-1]
+    print(text_reverse, text_reverse2)
 
 
-reverse_string("Hello World")
-reverse_string("Python")
+reverse_string("Hello", "World")
+reverse_string("Python", "")

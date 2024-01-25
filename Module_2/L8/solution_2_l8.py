b3af039951dcbfbfc6ac69b5dadff7c0bf71f2ec
Alexey Fatakhov <chesterik1922@yandex.ru>
2024-01-25 23:43:11 +0300

    fixed file with solution to tasks 2, 5

--- Module_2/L8/solution_2_l8.py
+++ Module_2/L8/solution_2_l8.py
@@ -3,5 +3,9 @@ word = input('Введите слово: ')
 
 if word in words:
     print('Синоним:', words[word])
+elif word in words.values():
+    for first in (words.keys()):
+        if words[first] == word:
+            print('Синоним:', first)
 else:
     print('Данного слова нет в списке.')
--- Module_2/L8/solution_5_l8.py
+++ Module_2/L8/solution_5_l8.py
@@ -1,11 +1,5 @@
-first_shop_input = input('Введите товары из первого магазина через запятую: ')
-first_shop = set(first_shop_input.split(', '))
-
-second_shop_input = input('Введите товары из второго магазина через запятую: ')
-second_shop = set(second_shop_input.split(', '))
-
-only_in_first = first_shop.difference(second_shop)
-only_in_second = second_shop.difference(first_shop)
-
-print(f"Только в первом магазине {only_in_first}")
-print(f"Только во втором магазине {only_in_second}")
+first_shop = set((input('Введите товары из первого магазина через запятую: ').split(", ")))
+second_shop = set((input('Введите товары из второго магазина через запятую: ').split(", ")))
+first_diff, second_diff = first_shop - second_shop, second_shop - first_shop
+print(f"Только в первом магазине: {first_diff}")
+print(f"Только во втором магазине: {second_diff}")

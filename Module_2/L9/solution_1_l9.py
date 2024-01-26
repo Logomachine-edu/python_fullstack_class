f8d1b1d6b84948c9303932fa14b822b594831e31
Alexey Fatakhov <chesterik1922@yandex.ru>
2024-01-27 00:29:50 +0300

    fixed file with solution to tasks 1, 2

--- Module_2/L9/solution_1_l9.py
+++ Module_2/L9/solution_1_l9.py
@@ -1,10 +1,8 @@
-price = list(map(int, input('Введите цены и скидку: ').split(", ")))
-
 def calculate_discount(price):
     prices = price[:-1]
     sum_prices = sum(prices)
     disc = sum_prices * (price[-1] / 100)
     return disc
 
-total = calculate_discount(price)
-print(f"Сумма скидки: {total}")
+print(f"Сумма скидки: {round(calculate_discount([100, 200, 300, 10]))}")
+print(f"Сумма скидки: {round(calculate_discount([50, 150, 250, 20]))}")
--- Module_2/L9/solution_2_l9.py
+++ Module_2/L9/solution_2_l9.py
@@ -1,8 +1,9 @@
-numbers = list(map(int, input('RGB: ').split(", ")))
+def convert_to_hex(R, G, B):
+    return "#{:02X}{:02X}{:02X}".format(R, G, B)
+r_hex = convert_to_hex(255, 0, 0)
+g_hex = convert_to_hex(0, 255, 0)
+b_hex = convert_to_hex(0, 0, 255)
 
-def convert_to_hex(numbers):
-    num_hex = f'#{numbers[0]:02X}{numbers[1]:02X}{numbers[2]:02X}'
-    return num_hex
-    
-hex_numbers = convert_to_hex(numbers)
-print(f"HEX: {hex_numbers}")
+print(f"HEX: {r_hex}")
+print(f"HEX: {g_hex}")
+print(f"HEX: {b_hex}")

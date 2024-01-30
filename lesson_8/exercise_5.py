725382be76c8d25b09247ee327445f9ed80c599a
Nizametdinov Bulat <nizametdinov.bulat@mail.ru>
2024-01-30 19:10:40 +0300

    eror correction

--- lesson_8/exercise_5.py
+++ lesson_8/exercise_5.py
@@ -1,12 +1,9 @@
-first_store_1 = {'Хлеб', 'Молоко', 'Сыр'}
-second_store_1 = {'Молоко', 'Йогурт', 'Сок'}
-first_store_2 = {'Кофе', 'Чай', 'Сахар'}
-second_store_2 = {'Какао', 'Чай', 'Сгущенка'}
-f1_store = first_store_1.difference(second_store_1)
-s1_store = second_store_1.difference(first_store_1)
-f2_store = first_store_2.difference(second_store_2)
-s2_store = second_store_2.difference(first_store_2)
-print('Только в первом магазине:', f1_store)
-print('Только во втором магазине:', s1_store)
-print('Только в первом магазине:', f2_store)
-print('Только во втором магазине:', s2_store)
+def store_name(products1, products2):
+    f1_store = products1.difference(products2)
+    f2_store = products2.difference(products1)
+    print('Только в первом магазине:', ", ".join(f1_store))
+    print('Только во втором магазине:', ", ".join(f2_store))
+
+
+store_name({'Хлеб', 'Молоко', 'Сыр'}, {'Молоко', 'Йогурт', 'Сок'})
+store_name({'Кофе', 'Чай', 'Сахар'}, {'Какао', 'Чай', 'Сгущенка'})

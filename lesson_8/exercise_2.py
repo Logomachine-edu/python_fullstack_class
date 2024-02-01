b1c31c3fd79ef713884d135296e416eada730d14
Nizametdinov Bulat <nizametdinov.bulat@mail.ru>
2024-02-01 19:32:03 +0300

    eror words

--- lesson_8/exercise_2.py
+++ lesson_8/exercise_2.py
@@ -6,12 +6,17 @@ print("Синоним:", words['Простой'])
 
 "Проверка на ошибку с добавлением в список новых синонимов"
 examination = input("Введите слово для проверки:")
-if examination not in words.keys():
-    print("Данного слова нет наличии")
-    words["new_key"] = examination
-    new_word = input("Введите для этого слова синоним:")
-    words[examination] = new_word
-    del words["new_key"]
-    print("Пример добавление в список новых синонимов:", words)
-else:
-    print("Синоним:", words[examination])
+for k, v in words.items():
+    if examination == v:
+        print(k)
+        break
+    elif examination == k:
+        print(v)
+        break
+    elif examination not in words.keys():
+        print("Данного слова нет наличии")
+        words["new_key"] = examination
+        new_word = input("Введите для этого слова синоним:")
+        words[examination] = new_word
+        del words["new_key"]
+        print("Пример добавление в список новых синонимов:", words)

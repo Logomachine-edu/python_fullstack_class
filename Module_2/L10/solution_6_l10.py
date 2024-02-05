6c417691ad326f0dd7fb8855a3fc76e55bfa9479
Alexey Fatakhov <chesterik1922@yandex.ru>
2024-02-05 22:38:04 +0300

    fixed file with solution to tasks 6

--- Module_2/L10/solution_6_l10.py
+++ Module_2/L10/solution_6_l10.py
@@ -1,18 +1,12 @@
 def find_max_price(prices):
     if len(prices) == 0:
         return None
-    elif len(prices) == 2:
-        if prices[0] > prices[1]:
-            return prices[0]
-        else:
-            return prices[1]
-    max_value = find_max_price(prices[1:])
-
-    if prices[0] > max_value:
+    elif len(prices) == 1:
         return prices[0]
     else:
-        return max_value
-    
+        max_price = find_max_price(prices[1:])
+        return prices[0] if prices[0] > max_price else max_price
+
 print(find_max_price([15, 7, 9]))
 print(find_max_price([1, 10, 20, 5]))
 print(find_max_price([25, 25, 25]))

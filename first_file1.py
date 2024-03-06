6b986243edf71b0149e3f48ba9ced48412894321
Sergei Polykov <polikovsergei6712@gmail.com>
2024-03-06 11:11:20 +0300

    1 developer finished!

--- first_file1.py
+++ first_file1.py
@@ -1,4 +1,7 @@
-# Мой первый код на Python
+#This program prints Hello, cats!
 print("Hello cats 1!")
 print("Hello cats 2!")
 print("Hello cats 3!")
+
+# Этот код работает как надо!
+# Автор Сергей Поляков

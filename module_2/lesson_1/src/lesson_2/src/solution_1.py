f7d59a90e3e59843e97643cf5f126e6b7268d7bf
Elemental6712 <150152272+Elemental6712@users.noreply.github.com>
2024-03-13 14:02:48 +0300

    Delete module_2/lesson_1/src/lesson_2/src directory

--- module_2/lesson_1/src/lesson_2/src/solution_1.py
+++
@@ -1,5 +0,0 @@
-price_type_str = input()
-prise_type_int = int(price_type_str)
-double_prise = int(prise_type_int) * 2
-double_prise = str(double_prise)
-print(double_prise, type(double_prise))
--- module_2/lesson_1/src/lesson_2/src/solution_3.py
+++
@@ -1,8 +0,0 @@
-book = int(input("Количество книг: "))
-stationery = int(input("Количество концтоваров: "))
-toys = int(input("Количество игрушек: "))
-volum_book = book * 2
-volum_stationery = stationery * 1.5
-volum_toys = toys * 3
-total_volume = volum_book + volum_stationery + volum_toys
-print(f"Общий объем: {total_volume} м^3")

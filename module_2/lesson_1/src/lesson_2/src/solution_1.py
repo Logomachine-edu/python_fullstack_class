019edc34d7e80b6353f195e82d9f4810213f51c7
Elemental6712 <150152272+Elemental6712@users.noreply.github.com>
2024-03-13 14:02:09 +0300

    Add files via upload

---
+++ module_2/lesson_1/src/lesson_2/src/solution_1.py
@@ -0,0 +1,5 @@
+price_type_str = input()
+prise_type_int = int(price_type_str)
+double_prise = int(prise_type_int) * 2
+double_prise = str(double_prise)
+print(double_prise, type(double_prise))
---
+++ module_2/lesson_1/src/lesson_2/src/solution_2.py
@@ -0,0 +1,5 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+arithmetic_averages = (first_number + second_number + third_number) / 3
+print(arithmetic_averages)

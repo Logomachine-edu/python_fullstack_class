47a98ed40626f727d2464fd14cfb39ca1d2ce25f
Elemental6712 <150152272+Elemental6712@users.noreply.github.com>
2024-03-13 14:08:24 +0300

    Delete module_2/lesson_2/src directory

--- module_2/lesson_2/src/solution_2.py
+++
@@ -1,5 +0,0 @@
-first_number = int(input())
-second_number = int(input())
-third_number = int(input())
-arithmetic_averages = (first_number + second_number + third_number) / 3
-print(arithmetic_averages)

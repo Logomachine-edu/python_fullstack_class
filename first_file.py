0c23fb2cb1c72c43a342f23d4ec418941f4f3c9f
Alex <alexeymihailow@mail.ru>
2024-03-17 16:10:10 +0300

    2 developer finished 2!

--- first_file.py
+++ first_file.py
@@ -1,7 +1,3 @@
-# This program prints Hello, cats!
-print('Hello, cats 1!')
-print('Hello, cats 2!')
-print('Hello, cats 3!')
-
-# Этот код работает как надо!
-# Автор Алекс
+# Мой первый код на Phyton
+print('Hello, dogs!')
+# => Hello, World!

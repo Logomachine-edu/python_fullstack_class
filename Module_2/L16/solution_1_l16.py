c49f3e8c1d00d3936a5130308e3945c1ae02e05a
Alexey Fatakhov <chesterik1922@yandex.ru>
2024-03-27 02:16:17 +0300

    fixed file with solution to tasks 1, 2, 6

--- Module_2/L16/solution_1_l16.py
+++ Module_2/L16/solution_1_l16.py
@@ -1,5 +1,4 @@
 def create_shelves():
-    num_shelves = int(input("Введите количество полок: "))
     shelf_lengths = list(map(int, input("Введите длины полок через запятую: ").split(',')))
 
     shelves = [[length, 0] for length in shelf_lengths]
--- Module_2/L16/solution_2_l16.py
+++ Module_2/L16/solution_2_l16.py
@@ -1,6 +1,5 @@
 def create_shelves():
-    num_shelves = int(input("Введите количество полок: "))
-    shelf_lengths = list(map(int, input("Введите длины полок через запятую: ").split(',')))
+    shelf_lengths = list(map(int, input("Введите полки через запятую: ").split(',')))
     items = list(map(int, input("Введите кол-во товаров через запятую: ").split(',')))
 
     shelves_goods = [[length, goods] for length, goods in zip(shelf_lengths, items)]
